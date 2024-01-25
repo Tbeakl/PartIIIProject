690dbba42d84141b1ad1f4578fa91457655d882a
Henry Batchelor <htb27@cam.ac.uk>
2024-01-25 01:06:56 +0000

    Added some functions for putting byte hamming weight leakages into the factor graph.

--- src/Julia/hamming_weight_probability_calculations.jl
+++ src/Julia/hamming_weight_probability_calculations.jl
@@ -1,3 +1,4 @@
+using Distributions
 
 function probability_of_choosing_without_replacement(number_in_input::Int64, number_set_in_input::Int64, number_chosen::Int64, number_set_in_output::Int64)
     if number_set_in_output > number_set_in_input
@@ -20,7 +21,7 @@ function probability_of_choosing_without_replacement(number_in_input::Int64, num
 end
 
 # Only support the splitting up of the parts when it is split evenly between the different parts
-function probability_of_hamming_values(standard_deviation::Float64, number_of_bits_in_input::Int64, number_of_bits_in_output::Int64, leakage_value::Float64)
+function likelihoods_of_hamming_values(standard_deviation::Float64, number_of_bits_in_input::Int64, number_of_bits_in_output::Int64, leakage_value::Float64)
     number_of_set_bits_in_original = 0:number_of_bits_in_input
     number_of_set_bits_in_output = 0:number_of_bits_in_output
 
@@ -31,5 +32,21 @@ function probability_of_hamming_values(standard_deviation::Float64, number_of_bi
     for i in number_of_set_bits_in_output
         likelihood_table[:, i+1] = likelihoods_of_set_bits .* probability_of_choosing_without_replacement.(number_of_bits_in_input, number_of_set_bits_in_original, number_of_bits_in_output, i)
     end
-    return sum(likelihood_table, dims=1)
+    return sum(likelihood_table, dims=1)[1, :]
+end
+
+function table_for_hamming_values(number_of_input_bits::Int64)
+    hamming_table = zeros(Bool, number_of_input_bits + 1, 1 << number_of_input_bits)
+    for i in 1:(1 << number_of_input_bits)
+        hamming_table[Base.count_ones(i - 1) + 1, i] = true
+    end
+    return hamming_table
+end
+
+function make_prob_distribution_from_hamming_likelihoods(likelihoods_of_hamming_values::Vector{Float64}, table_of_hamming_values::Matrix{Bool}, number_of_bits::Int64)
+    prob_distribution = zeros(1 << number_of_bits)
+    for i in 1:(number_of_bits + 1)
+        prob_distribution[table_for_hamming_values[i, :]] = likelihoods_of_hamming_values[i]
+    end
+    return prob_distribution ./ sum(prob_distribution)
 end
--- src/Julia/noisy_byte_hamming_weight_traces.jl
+++ src/Julia/noisy_byte_hamming_weight_traces.jl
@@ -1,6 +1,8 @@
 using CryptoSideChannel
 using Distributions
 include("chacha.jl")
+include("node.jl")
+include("hamming_weight_probability_calculations.jl")
 
 σ = 0.1
 
@@ -38,6 +40,106 @@ function encrypt_collect_trace(key::Vector{UInt32}, nonce::Vector{UInt32}, count
     return copy(trace)
 end
 
+# Currently need the clusters to fall exactly along with the leakages
+function add_distribution_from_position_in_trace(trace::Vector{Any},
+    variables::Dict{String, Variable},
+    factors::Dict{String, Factor},
+    bits_per_cluster::Int64,
+    location_execution_counts::Vector{Int64},
+    variable::Int64,
+    hamming_position_table::Matrix{Bool},
+    standard_deviation::Float64
+    )
+    global position_in_trace
+    
+    clusters_per_leakage_weight = Int64(ceil(8 / bits_per_cluster))
+    for i in 1:length(trace[position_in_trace])
+        hamming_value_likelihoods = likelihoods_of_hamming_values(standard_deviation, 8, bits_per_cluster, trace[position_in_trace][i])
+        prob_dist_for_cluster = make_prob_distribution_from_hamming_likelihoods(hamming_value_likelihoods, hamming_position_table, bits_per_cluster)
+        for j in 1:clusters_per_leakage_weight
+            cur_var_name = string(variable, "_", location_execution_counts[variable], "_", (i - 1) * clusters_per_leakage_weight + j)
+            cur_dist_name = string("f_", cur_var_name, "_dist")
+            factors[cur_dist_name] = Factor(cur_dist_name, LabelledArray(prob_dist_for_cluster, [cur_var_name]))
+            add_edge_between(variables[cur_var_name], factors[cur_dist_name])
+        end
+    end
+    
+    # Need to pay attention to how to deal with rotations
+    position_in_trace += 1
+    location_execution_counts[variable] += 1
+end
+
+function add_qr_trace(trace::Vector{Any},
+    variables::Dict{String, Variable},
+    factors::Dict{String, Factor},
+    bits_per_cluster::Int64,
+    a::Int64,
+    b::Int64,
+    c::Int64,
+    d::Int64,
+    location_execution_counts::Vector{Int64},
+    hamming_position_table::Matrix{Bool},
+    standard_deviation::Float64
+    )
+    global position_in_trace
+
+    add_distribution_from_position_in_trace(trace, variables, factors, bits_per_cluster, location_execution_counts, a, hamming_position_table, standard_deviation)
+    # Need to do the part with shifting
+    position_in_trace += 3
+    if 16 % bits_per_cluster != 0
+        # This means that the result of the rotation is directly going to be the output after the rotation
+        location_execution_counts[d] += 1 
+    end
+    add_distribution_from_position_in_trace(trace, variables, factors, bits_per_cluster, location_execution_counts, d, hamming_position_table, standard_deviation)
+
+    add_distribution_from_position_in_trace(trace, variables, factors, bits_per_cluster, location_execution_counts, c, hamming_position_table, standard_deviation)
+    position_in_trace += 3
+    if 12 % bits_per_cluster != 0
+        location_execution_counts[b] += 1
+    end
+    add_distribution_from_position_in_trace(trace, variables, factors, bits_per_cluster, location_execution_counts, b, hamming_position_table, standard_deviation)
+
+    add_distribution_from_position_in_trace(trace, variables, factors, bits_per_cluster, location_execution_counts, a, hamming_position_table, standard_deviation)
+    # Need to do the part with shifting
+    position_in_trace += 3
+    if 8 % bits_per_cluster != 0
+        # This means that the result of the rotation is directly going to be the output after the rotation
+        location_execution_counts[d] += 1 
+    end
+    add_distribution_from_position_in_trace(trace, variables, factors, bits_per_cluster, location_execution_counts, d, hamming_position_table, standard_deviation)
+
+    add_distribution_from_position_in_trace(trace, variables, factors, bits_per_cluster, location_execution_counts, c, hamming_position_table, standard_deviation)
+    position_in_trace += 3
+    if 7 % bits_per_cluster != 0
+        location_execution_counts[b] += 1
+    end
+    add_distribution_from_position_in_trace(trace, variables, factors, bits_per_cluster, location_execution_counts, b, hamming_position_table, standard_deviation)
+end
+
+function add_trace_to_factor_graph(trace::Vector{Any},
+    variables::Dict{String, Variable},
+    factors::Dict{String, Factor},
+    bits_per_cluster::Int64,
+    standard_deviation::Float64
+    )
+    global position_in_trace
+    position_in_trace = 1
+    location_execution_counts = ones(Int64, 16)
+    hamming_position_table = table_for_hamming_values(bits_per_cluster)
+    for i in 1:10
+        add_qr_trace(trace, variables, factors, bits_per_cluster, 1, 5, 9, 13, location_execution_counts, hamming_position_table, standard_deviation)
+        add_qr_trace(trace, variables, factors, bits_per_cluster, 2, 6, 10, 14, location_execution_counts, hamming_position_table, standard_deviation)
+        add_qr_trace(trace, variables, factors, bits_per_cluster, 3, 7, 11, 15, location_execution_counts, hamming_position_table, standard_deviation)
+        add_qr_trace(trace, variables, factors, bits_per_cluster, 4, 8, 12, 16, location_execution_counts, hamming_position_table, standard_deviation)
+
+        add_qr_trace(trace, variables, factors, bits_per_cluster, 1, 6, 11, 16, location_execution_counts, hamming_position_table, standard_deviation)
+        add_qr_trace(trace, variables, factors, bits_per_cluster, 2, 7, 12, 13, location_execution_counts, hamming_position_table, standard_deviation)
+        add_qr_trace(trace, variables, factors, bits_per_cluster, 3, 8, 9, 14, location_execution_counts, hamming_position_table, standard_deviation)
+        add_qr_trace(trace, variables, factors, bits_per_cluster, 4, 5, 10, 15, location_execution_counts, hamming_position_table, standard_deviation)
+    end
+    # Need to have another part for setting the values to be exactly what they were in the output of the function because
+    # we assume that we have access to those actual values compared to just their leakage values
+end
 
 # How the full trace (1616) is broken down
 
