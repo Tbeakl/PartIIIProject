28cfec90cd43e90a1cfa49a8ece509184ef88c4c
Henry Batchelor <henry.batchelor@hotmail.co.uk>
2024-05-18 02:41:34 +0100

    Started to update plotting amount of enumeration to have top lines

--- src/Julia/evaluation/plot_successes_against_amount_of_enumeration_simulation.jl
+++ src/Julia/evaluation/plot_successes_against_amount_of_enumeration_simulation.jl
@@ -1,5 +1,50 @@
 using Plots, HDF5
 
+path_to_data = "C:/Users/henry/Documents/PartIIIProject/data/"
+base_path_to_counts = path_to_data * "evaluation/simulation_known_output_nonce_counter/"
+
+signal_to_noise_ratios = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6]
+
+initial_ranks::Dict{Number,Vector{Number}} = Dict()
+final_ranks::Dict{Number,Vector{Number}} = Dict()
+
+for snr in signal_to_noise_ratios
+    current_initial_ranks::Vector{Float64} = []
+    current_final_ranks::Vector{Float64} = []
+    fid = h5open(string(base_path_to_counts, snr, "_successes.hdf5"), "r")
+    for i in 1:100
+        push!(current_initial_ranks, read(fid[string("initial_estimated_rank_log2_", i)]))
+        push!(current_final_ranks, read(fid[string("final_estimated_rank_log2_", i)]))
+    end
+    close(fid)
+    initial_ranks[snr] = sort(current_initial_ranks)
+    final_ranks[snr] = sort(current_final_ranks)
+end
+
+p = plot(size=(1500, 500),
+    title="Proportion of keys successfully found after differing amounts of key enumeration",
+    ylabel="Proportion",
+    xlabel="Log base 2 of estimated number of keys required to be enumerated",
+    leftmargin=8Plots.mm,
+    bottom_margin=6Plots.mm,
+    legend=:outerright, legendcolumns=1, xlim=(0, 256), ylim=(0, 1))
+proportion = collect((1:100) ./ 100)
+cur_colors = get_color_palette(:auto, plot_color(:white))
+
+# Add on a final value at the top left of each so that the lines continue along the top
+push!(proportion, 1.0)
+for i in eachindex(signal_to_noise_ratios)
+    push!(initial_ranks[signal_to_noise_ratios[i]], 256)
+    push!(final_ranks[signal_to_noise_ratios[i]], 256)
+end
+
+for i in eachindex(signal_to_noise_ratios)
+    plot!(p, initial_ranks[signal_to_noise_ratios[i]], proportion, c=cur_colors[i], linestyle=:dash, label=string("Pre-SASCA ", signal_to_noise_ratios[i], " SNR"), linewidth=1.5)
+    plot!(p, final_ranks[signal_to_noise_ratios[i]], proportion, c=cur_colors[i], linestyle=:solid, label=string("Post-SASCA ", signal_to_noise_ratios[i], " SNR"), linewidth=1.5)
+end
+p
+savefig(p, "./plots/evaluation/simulated_signal_to_noise_ratios_known_output.pdf")
+
 
 path_to_data = "C:/Users/henry/Documents/PartIIIProject/data/"
 base_path_to_counts = path_to_data * "evaluation/simulation_unknown_output_nonce_counter/"
@@ -28,11 +73,20 @@ p = plot(size=(1500, 500),
     xlabel="Log base 2 of estimated number of keys required to be enumerated",
     leftmargin=8Plots.mm,
     bottom_margin=6Plots.mm,
-    legend=:outerright, legendcolumns=1, xlim=(0,256), ylim=(0,1))
-proportion = (1:100) ./ 100
+    legend=:outerright, legendcolumns=1, xlim=(0, 256), ylim=(0, 1))
+proportion = collect((1:100) ./ 100)
 cur_colors = get_color_palette(:auto, plot_color(:white))
+
+# Add on a final value at the top left of each so that the lines continue along the top
+push!(proportion, 1.0)
 for i in eachindex(signal_to_noise_ratios)
-    plot!(p, initial_ranks[signal_to_noise_ratios[i]], proportion, c=cur_colors[i], linestyle=:dash, label=string("Pre-SASCA ", signal_to_noise_ratios[i]))
-    plot!(p, final_ranks[signal_to_noise_ratios[i]], proportion, c=cur_colors[i], linestyle=:solid, label=string("Post-SASCA ", signal_to_noise_ratios[i]))
+    push!(initial_ranks[signal_to_noise_ratios[i]], 256)
+    push!(final_ranks[signal_to_noise_ratios[i]], 256)
 end
+
+for i in eachindex(signal_to_noise_ratios)
+    plot!(p, initial_ranks[signal_to_noise_ratios[i]], proportion, c=cur_colors[i], linestyle=:dash, label=string("Pre-SASCA ", signal_to_noise_ratios[i], " SNR"), linewidth=1.5)
+    plot!(p, final_ranks[signal_to_noise_ratios[i]], proportion, c=cur_colors[i], linestyle=:solid, label=string("Post-SASCA ", signal_to_noise_ratios[i], " SNR"), linewidth=1.5)
+end
+p
 savefig(p, "./plots/evaluation/simulated_signal_to_noise_ratios_unknown_output.pdf")
---
+++ src/Julia/evaluation/plot_successes_against_amount_of_enumeration_single_trace_post_SASCA.jl
@@ -0,0 +1,125 @@
+using Plots, HDF5
+include("../encryption/key_enumeration.jl")
+gr()
+base_path_to_data = "C:/Users/henry/Documents/PartIIIProject/data/"
+
+# Traces which have NaNs in them 347, 384, 403 need to potentially look in more detail at why they do and 
+# if there is someway they can be improved
+base_paths_to_counts::Vector{String} = base_path_to_data .* [
+    "evaluation/attack_8_on_32_known/",
+    "evaluation/attack_8_on_32_unknown/",
+    "evaluation/random_counter_1_8/",
+    "evaluation/random_counter_1_16/",
+    "evaluation/random_counter_32_volatile_1_8/",
+]
+
+# "evaluation/random_counter_incremented_10_8/",
+# "evaluation/random_counter_incremented_10_16/",
+# "evaluation/incremented_counter_prod_10_8/",
+# "evaluation/incremented_counter_prod_10_16/",
+# "evaluation/set_counter_mean_10_8/",
+# "evaluation/set_counter_mean_10_16/",
+# "evaluation/set_counter_prod_10_8/",
+# "evaluation/set_counter_prod_10_16/",
+# "evaluation/random_counter_32_volatile_set_mean_10_8/",
+# "evaluation/random_counter_32_volatile_set_prod_10_8/"
+
+paths_to_actual_keys::Vector{String} = base_path_to_data .* "captures/" .* [
+    "ChaChaRecordings_8_on_32/recording_attack_counter_from_random_",
+    "ChaChaRecordings_8_on_32/recording_attack_counter_from_random_",
+    "ChaChaRecordings_2/recording_attack_counter_from_random_",
+    "ChaChaRecordings_2/recording_attack_counter_from_random_",
+    "ChaChaRecordings_3/recording_attack_counter_from_random_",
+]
+
+# "ChaChaRecordings_2/recording_attack_counter_from_random_",
+#     "ChaChaRecordings_2/recording_attack_counter_from_random_",
+#     "ChaChaRecordings_2/recording_attack_counter_from_random_",
+#     "ChaChaRecordings_2/recording_attack_counter_from_random_",
+#     "ChaChaRecordings_2/recording_attack_counter_constant_",
+#     "ChaChaRecordings_2/recording_attack_counter_constant_",
+#     "ChaChaRecordings_2/recording_attack_counter_constant_",
+#     "ChaChaRecordings_2/recording_attack_counter_constant_",
+#     "ChaChaRecordings_3/recording_attack_counter_constant_",
+#     "ChaChaRecordings_3/recording_attack_counter_constant_",
+
+final_ranks::Vector{Vector{Number}} = []
+
+labels::Vector{String} = ["8-bit implementation\nsingle trace",
+    "8-bit implementation\nunknown counter, nonce, output",
+    "8-bit fragment\nsingle trace",
+    "16-bit fragment\nsingle trace",
+    "8-bit fragment volatile\nsingle trace",]
+
+# "8-bit fragment 10 trace\nchanged counter mean",
+# "16-bit fragment 10 trace\nchanged counter mean",
+# "8-bit fragment 10 trace\nchanged counter product",
+# "16-bit fragment 10 trace\nchanged counter product",
+# "8-bit fragment 10 trace\nset counter mean",
+# "16-bit fragment 10 trace\nset counter mean",
+# "8-bit fragment 10 trace\nset counter product",
+# "16-bit fragment 10 trace\nset counter product",
+# "8-bit fragment volatile 10 trace\nset counter mean",
+# "8-bit fragment voaltile 10 trace\nset counter product"
+
+for (i, base_path_to_counts) in enumerate(base_paths_to_counts)
+    current_final_ranks::Vector{Number} = []
+    for trace_number in 1:1000
+        if ispath(string(base_path_to_counts, trace_number, ".hdf5"))
+            fid = h5open(string(base_path_to_counts, trace_number, ".hdf5"), "r")
+            # Need to do all of the different types of real key enumeration, possibly need to increase
+            # the number of iterations done on the unknown output version because it may not be correct
+            current_estimated_rank = read(fid[string("final_estimated_rank_log2")])
+            if current_estimated_rank < 20
+                # Actually perform the key enumeration to find the solution
+                # need to also get the key for this to know the correct values which is not stored in it 
+                prob_tables = read(fid[string("final_likelihood_tables")])
+                # Also need to read in the correct key so need to pick the correct file and trace within it
+                # println(trace_number)
+                file_number = (trace_number ÷ 100)
+                trace_number_in_file = (trace_number - 1) % 100
+                key_fid = h5open(paths_to_actual_keys[i] * string(file_number) * ".hdf5", "r")
+                key = UInt32.(read(key_fid["power_"*string(trace_number_in_file)*"_0"]["key"]))
+                close(key_fid)
+
+                sorted_likelihood_matrix, sorted_values = make_matrices_of_values(eachrow(prob_tables))
+                key_by_cluster = turn_key_into_cluster_values(key, 8)
+                actual_rank = key_enumerate(sorted_likelihood_matrix, sorted_values, key_by_cluster, 1 << 20)
+                if actual_rank >= (1 << 20)
+                    println(base_path_to_counts, ": ", trace_number)
+                    println(calculate_log_likelihood_of_key(key, eachrow(prob_tables), 8))
+                    println(current_estimated_rank)
+                end
+                push!(current_final_ranks, log2(actual_rank))
+            else
+                push!(current_final_ranks, current_estimated_rank)
+            end
+            if isnan(read(fid["entropy_over_time"])[end])
+                println(base_path_to_counts, " ", trace_number)
+            end
+            close(fid)
+        end
+    end
+    push!(final_ranks, sort(current_final_ranks))
+end
+
+p = plot(size=(1500, 500),
+    title="Proportion of keys successfully found after differing amounts of key enumeration",
+    ylabel="Proportion",
+    xlabel="Log base 2 of estimated number of keys required to be enumerated",
+    leftmargin=8Plots.mm,
+    bottom_margin=6Plots.mm,
+    legend=:outerright, legendcolumns=1, xlim=(0, 256), ylim=(0, 1))
+proportion = collect((1:1000) ./ 1000)
+
+push!(proportion, 1.0)
+for i in eachindex(base_paths_to_counts)
+    push!(final_ranks[i], 256)
+end
+
+for i in eachindex(base_paths_to_counts)
+    cur_colors = get_color_palette(:auto, plot_color(:white))
+    plot!(p, final_ranks[i], proportion, label=labels[i], linewidth=1.5)
+end
+p
+savefig(p, "./plots/evaluation/real_attack_single_trace_post_SASCA.pdf")
